b2a3abf24751217e763b6b41a5f4110848b9ec1d
Guilherme <guilherme_campiotto@hotmail.com>
2018-09-04 21:21:26 -0300

    Transferindo assets utilizados no projeto.

---
+++ Assets/Level Design/Scripts/ThunderClap.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThunderClap : MonoBehaviour {
+
+    public AudioClip thunderClip;
+    private bool canFlicker = true;
+	
+	void Update () {
+        StartCoroutine(Flicker());
+	}
+
+    /*
+     * Ativa a luz do objeto para simular o clarão dos raios de uma tempestade e toca o som do trovão.
+     */
+    IEnumerator Flicker() {
+        if (canFlicker) {
+            canFlicker = false;
+            GetComponent<AudioSource>().PlayOneShot(thunderClip);
+            GetComponent<Light>().enabled = true;
+            yield return new WaitForSeconds(Random.Range(0.1f, 0.4f));
+            GetComponent<Light>().enabled = false;
+            yield return new WaitForSeconds(Random.Range(0.1f, 5));
+            canFlicker = true;
+        }
+    }
+}
